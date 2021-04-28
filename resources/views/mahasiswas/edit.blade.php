0185d7e56e6a25f175bb70646b0be5728c1cd3b0
MuhammadDhiyaulAuliya <59078451+MuhammadDhiyaul@users.noreply.github.com>
2021-04-29 02:31:13 +0700

    07&09_Pemrograman_Web_Lanjut

--- resources/views/mahasiswas/edit.blade.php
+++ resources/views/mahasiswas/edit.blade.php
@@ -31,6 +31,7 @@
                <input type="text" name="nama" class="form-control" id="Nama" value="{{ $mahasiswa->nama }}" aria describedby="Nama" >
             </div>
             <div class="form-group">
+               <label for="kelas">Kelas</label>
                <select name="kelas" class="form-control">
                   @foreach ($kelas as $item)
                   <option value="{{$item->id}}" {{$mahasiswa->kelas_id == $item->id ? 'selected': ''}}>
